0c41ebf5d8f095513d93be93577b2354f47d5c42
@hexlet/cli <support@hexlet.io>
2024-09-21 13:03:35 +0300

    submit java-oop-ru/classes-and-objects

---
+++ java-oop-ru/classes-and-objects/src/main/java/exercise/Point.java
@@ -0,0 +1,15 @@
+package exercise;
+
+import lombok.Getter;
+
+@Getter
+public class Point {
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
---
+++ java-oop-ru/classes-and-objects/src/main/java/exercise/Segment.java
@@ -0,0 +1,21 @@
+package exercise;
+
+import lombok.Getter;
+
+@Getter
+public class Segment {
+    private Point beginPoint;
+    private Point endPoint;
+    private Point midPoint;
+
+    public Segment(Point begin, Point end) {
+        beginPoint = begin;
+        endPoint = end;
+    }
+
+    public Point getMidPoint() {
+        var midPointX = beginPoint.getX() + endPoint.getX() / 2;
+        var midPointY = beginPoint.getY() + endPoint.getY() / 2;
+        return new Point(midPointX, midPointY);
+    }
+}
