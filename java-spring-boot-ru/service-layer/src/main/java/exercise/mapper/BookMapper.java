04962977352a49839caafa0d67712b0b6ed56c1e
@hexlet/cli <support@hexlet.io>
2025-01-07 09:08:38 +0300

    submit java-spring-boot-ru/service-layer

--- java-spring-boot-ru/service-layer/src/main/java/exercise/mapper/BookMapper.java
+++ java-spring-boot-ru/service-layer/src/main/java/exercise/mapper/BookMapper.java
@@ -20,7 +20,13 @@ import org.mapstruct.ReportingPolicy;
 public abstract class BookMapper {
 
     // BEGIN
-    
+    @Mapping(target = "authorId", source = "author.id")
+    @Mapping(target = "authorFirstName", source = "author.firstName")
+    @Mapping(target = "authorLastName", source = "author.lastName")
+    public abstract BookDTO map(Book model);
+
+    @Mapping(target = "author", source = "authorId")
+    public abstract Book map(BookCreateDTO dto);
     // END
 
     @Mapping(target = "author", source = "authorId")
